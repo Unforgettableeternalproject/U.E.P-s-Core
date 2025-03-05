b346760d269a60ad15cc08eec341d56cb1fc3ad7
Bernie <ptyc4076@gmail.com>
2025-03-05 20:34:26 +0800

    Added a githook for changelog management. 03/05

--- Entry.py
+++ Entry.py
@@ -1,1 +1,10 @@
+import sys, os, time
+
+if __name__ == "__main__":
+    print("Hello World!")
+    time.sleep(2)
+    print("Goodbye World!")
+    time.sleep(2)
+    sys.exit(0)
+
 # This will be the entry file for the program.
